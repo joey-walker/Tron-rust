22f72f69f47a1c1f7ccc8dc5486e6d17efc82f65
Joey Walker <walkerj1@wit.edu>
2015-06-25 00:57:22 -0400

    More work on tron project, tried some cleanup and preparation for collision detection. Added .dll to allow it to actually compile.

--- src/main.rs
+++ src/main.rs
@@ -8,124 +8,163 @@
 
 extern crate piston;
 extern crate graphics;
-extern crate glutin_window;
+extern crate sdl2_window;
 extern crate opengl_graphics;
 
 use piston::window::WindowSettings;
 use piston::event::*;
-use glutin_window::GlutinWindow as Window;
+use sdl2_window::Sdl2Window as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 use piston::input::{ Button, Key};
+use graphics::*;
+
+// Our player object
+pub struct Player {
+  x: f64, // our initial xlocation
+  y: f64, // our initial ylocation
+  color: [f32; 4], // color of player, example: .. = [0.0, 1.0, 0.0, 1.0];
+  playerobj : types::Rectangle
+}
 
 pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-   // rotation: f64,   // Rotation for the square.
-    translatex: f64, //translate square
-    translatey: f64 //translate square
+  gl: GlGraphics, // OpenGL drawing backend.
+
+  player1: Player, 
+
+  player2: Player
 }
 
 impl App {
-	fn render(&mut self, args: &RenderArgs) {
-		use graphics::*;
+ fn render(&mut self, args: &RenderArgs) {
 
-       // const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-       const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-       const BLUE:	 [f32; 4] = [0.30, 0.30, 1.0, 1.0];
 
-       let square = rectangle::square(0.0, 0.0, 25.0);
-       //let rotation = self.rotation;
-       let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
+ // const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+     //const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+     const BLUE:	 [f32; 4] = [0.30, 0.30, 1.0, 1.0];
 
-       let translatex = self.translatex;
-       let translatey = self.translatey;
+     //let rotation = self.rotation;
 
-       self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(BLUE, gl);
+     //player 1
 
-           /* let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-                                       */
-                                       let transform = c.transform.trans(translatex,translatey);
+     let (player1x, player1y) = (self.player1.x, self.player1.y); // initial coordinates
+     let player1_color = self.player1.color; //player's color
+     let player1_obj = self.player1.playerobj; //player object, currently rectangle
 
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+     //player 2 
+
+     let (player2x, player2y) = (self.player2.x, self.player2.y);
+     let player2_color = self.player2.color;
+     let player2_obj = self.player2.playerobj;
+
+    //draw the scene
+     self.gl.draw(args.viewport(), |c, gl| {
+          // Clear the screen.
+          clear(BLUE, gl);
+
+         // let transform = c.transform.trans(x, y).rot_rad(rotation).trans(-25.0, -25.0);
+
+         //coordinates of our player
+         let transform = c.transform.trans(player1x,player1y);
+         let transform1 = c.transform.trans(player2x,player2y);
+
+          // Draw a box rotating around the middle of the screen.
+          rectangle(player1_color, player1_obj, transform, gl);
+
+          //second player
+          rectangle(player2_color, player2_obj, transform1, gl);
         });
+     
    }
 
    fn update(&mut self, args: &UpdateArgs, xspeed:f64,yspeed:f64) {
 
-    	//args.dt?  <- What exactly is this?
+  	//args.dt?  <- What exactly is this? <- Delta time.
 
-    	//Move our square
-    	self.translatex += xspeed * args.dt;
-    	self.translatey += yspeed * args.dt;
-        // Rotate 2 radians per second.
-       // self.rotation += 2.0 * args.dt;
+  	//Move our square
+  	self.player1.x += xspeed * args.dt;
+  	self.player1.y += yspeed * args.dt;
+      // Rotate 2 radians per second.
+     // self.rotation += 2.0 * args.dt;
 
+   }
+ }
+
+ fn main() {
+   let opengl = OpenGL::_3_2;
+
+
+   let mut xspeed = 80.0;
+   let mut yspeed = 0.0;
+
+  // Create a window.
+  let window = Window::new(
+  	opengl,
+  	WindowSettings::new(
+  		"Tron",
+  		[1000, 800]
+  		)
+  	.exit_on_esc(true)
+    .fullscreen(false)
+    .vsync(false),
+    );
+
+  // Create a new game and run it.
+  let mut app = App {
+  	gl: GlGraphics::new(opengl),
+
+     player1: Player { //create our player
+      x: 0.0,
+      y: 0.0,
+      color: [1.0, 0.0, 0.0, 1.0],
+      playerobj: rectangle::square(0.0, 0.0, 25.0)
+    },
+
+     player2: Player { //create our player
+      x: 600.0,
+      y: 600.0,
+      color: [0.0, 1.0, 0.0, 1.0],
+      playerobj: rectangle::square(0.0, 0.0, 25.0)
     }
-}
 
-fn main() {
-	let opengl = OpenGL::_3_2;
+  };
 
+  //our event loop.
+  for e in window.events() {
+  	if let Some(r) = e.render_args() {
+  		app.render(&r);
+  	}
 
-	let mut xspeed = 80.0;
-	let mut yspeed = 0.0;
+  	if let Some(u) = e.update_args() {
+  		app.update(&u, xspeed,yspeed);
 
-    // Create an Glutin window.
-    let window = Window::new(
-    	opengl,
-    	WindowSettings::new(
-    		"Tron",
-    		[1000, 800]
-    		)
-    	.exit_on_esc(true)
-    	);
+  	}
 
-    // Create a new game and run it.
-    let mut app = App {
-    	gl: GlGraphics::new(opengl),
-    	//rotation: 0.0,
-        translatey: 0.0, //start 
-        translatex: 0.0
-    };
+      //Keyboard Events
+      if let Some(Button::Keyboard(key)) = e.press_args() {
+      	match key {
+      		Key::S => {
+      			println!("You pressed S :: heading down");
+      			yspeed = 80.0;
+      			xspeed =0.0;
+      		},
+      		Key::W => {
+      			println!("You pressed W :: heading up");
+      			yspeed = -80.0; 
+      			xspeed =0.0;
+      		},
+      		Key::A => {
+      			println!("You pressed A :: heading left");
+      			yspeed = 0.0; 
+      			xspeed = -80.0;
+      		},
+      		Key::D => {
+      			println!("You pressed D :: heading right");
+      			yspeed = 0.0; 
+      			xspeed = 80.0;
+      		},
+      		_ => println!("You pressed {:?} . No Change", key),
+      	}
 
-    for e in window.events() {
-    	if let Some(r) = e.render_args() {
-    		app.render(&r);
-    	}
-
-    	if let Some(u) = e.update_args() {
-    		app.update(&u, xspeed,yspeed);
-    	}
-
-        //Keyboard Events
-        if let Some(Button::Keyboard(key)) = e.press_args() {
-        	match key {
-        		Key::S => {
-        			println!("You pressed S :: heading down");
-        			yspeed = 80.0;
-        			xspeed =0.0;
-        		},
-        		Key::W => {
-        			println!("You pressed W :: heading up");
-        			yspeed = -80.0; 
-        			xspeed =0.0;
-        		},
-        		Key::A => {
-        			println!("You pressed A :: heading left");
-        			yspeed = 0.0; 
-        			xspeed = -80.0;
-        		},
-        		Key::D => {
-        			println!("You pressed D :: heading right");
-        			yspeed = 0.0; 
-        			xspeed = 80.0;
-        		},
-        		_ => println!("You pressed {:?} . No Change", key),
-        	}
-        }
+      }
     }
-}
+  }
